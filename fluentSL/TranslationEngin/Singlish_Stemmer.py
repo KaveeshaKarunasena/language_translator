79f1cdcfacd716a784dbc4f5c617040c4f95ece6
kaveesha karunasena <kaveeshalankeshwara2001@gmail.com>
2023-10-12 01:41:35 +0530

    mongodb full text search

--- fluentSL/TranslationEngin/Singlish_Stemmer.py
+++ fluentSL/TranslationEngin/Singlish_Stemmer.py
@@ -2,13 +2,35 @@ import sys
 import numpy as np
 from numpy.linalg import norm
 from fuzzywuzzy import fuzz
+from main import db_connection
 
-example_words = ["apita","heta","eakak","thiyenwa","exam"]
+# example_words = ["apita","heta","eakak","thiyenwa","exam"]
 
 
-n1 = "gedara"
-n2 = "gedaraka"
+# n1 = "gedara"
+# n2 = "gedaraka"
 
-print(fuzz.ratio(n1, n2))
+# print(fuzz.ratio(n1, n2))
+
+db = db_connection()
+test_db = db.test
+dictionary = test_db.dictionary
+
+def fuzzy_singlish_matching():
+    result = dictionary.aggregate([
+        {
+            "$search":{
+                "index":"language_index",
+                "text":{
+                    "query":"rupaya",
+                    "path":"singlish_word",
+                    "fuzzy":{}
+                }
+            }
+        }
+    ])
+    print(list(result))
+
+fuzzy_singlish_matching()
 
 
--- fluentSL/TranslationEngin/main.py
+++ fluentSL/TranslationEngin/main.py
@@ -1,17 +1,12 @@
-from sysconfig import get_python_version
+
 from dotenv import load_dotenv,find_dotenv
 import os
-import pprint
 from pymongo import MongoClient
-import English_Stemmer
 load_dotenv(find_dotenv())
 
-connection_string = os.environ.get("DB_URI")
+def db_connection():
+    connection_string = os.environ.get("DB_URI")
 
-client = MongoClient(connection_string)
-
-dbs = client.list_database_names()
-test_db = client.test
-collection = test_db.test
-
-print(dbs)
+    client = MongoClient(connection_string)
+    return client
+    
